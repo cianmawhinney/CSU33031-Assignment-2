7396d098a95ee0bbe3ea2fcb5f8388348ebbf04a
Cian Mawhinney <cianmawhinney@gmail.com>
2021-11-22 20:03:03 +0000

    Get a basic controller running

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,17 @@
 'use strict';
 
-// const dgram = require('dgram');
+const dgram = require('dgram');
+const Protocol = require('flow-protocol');
 
-console.log("Hi, I'm the controller");
+const LISTENING_PORT = 51510;
+
+const server = dgram.createSocket('udp4');
+server.bind(LISTENING_PORT, () => {
+  console.log('Controller up!');
+});
+
+const p = new Protocol(server);
+
+p.on('applicationRegistration', (packet) => {
+  console.log(packet);
+});
